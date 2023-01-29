641495ab23a514d4097c191f6438c1bb716b1725
wabibaah <wabisernproject@gmail.com>
2023-01-29 06:21:41 +0000

    done with the contact us page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,112 @@
 import React from "react";
+import Meta from "../components/Meta";
+import BreadCrumb from "../components/BreadCrumb";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <>
+      <Meta title="Contact Us" />
+      <BreadCrumb title="Contact Us" />
+      <div className="contact-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <iframe
+                src=""
+                frameborder="0"
+                width="600"
+                height="400"
+                className="border-0 w-100"
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                allowFullScreen=""
+              ></iframe>
+            </div>
+            <div className="col-12 mt-5">
+              <div className="contact-inner-wrapper d-flex justify-content-between">
+                <div className="">
+                  <h3 className="contact-title mb-4">Contact</h3>
+                  <form className="d-flex flex-column gap-15" action="">
+                    <div>
+                      <input
+                        type="text"
+                        name=""
+                        id=""
+                        className="form-control"
+                        placeholder="Name"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="email"
+                        name=""
+                        id=""
+                        className="form-control"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <input
+                        type="tel"
+                        name=""
+                        id=""
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        className="w-100 form-control"
+                        name=""
+                        id=""
+                        cols="30"
+                        rows="4"
+                        placeholder="Message"
+                      ></textarea>
+                    </div>
+                    <div>
+                      <button type="submit" className="button border-0">
+                        Submit
+                      </button>
+                    </div>
+                  </form>
+                </div>
+                <div className="">
+                  <h3 className="contact-title mb-4">Get In Touch With Us</h3>
+                  <div className="">
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5" />
+                        <address className="mb-0">123, Amasaman New Road</address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a className="mb-0" href="tel:+233244639729">
+                          Call us on (+233) 024463929
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a className="mb-0" href="mailto:wabisernprojects@gmail.com">
+                          Email Us on Wabisernprojects@gmail.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday - Friday 10AM - 8PM</p>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Contact;
